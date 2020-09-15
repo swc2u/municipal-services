2f7811906f61362f50d83158ab47987de7f66284
pooja-transerve <pooja.p@transerve.com>
2020-09-15 11:28:35 +0530

    Rent data read from excel

--- rented-properties/src/main/java/org/egov/cpt/web/controllers/ReadExcelController.java
+++ rented-properties/src/main/java/org/egov/cpt/web/controllers/ReadExcelController.java
@@ -1,10 +1,15 @@
 package org.egov.cpt.web.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import javax.validation.Valid;
 
 import org.egov.cpt.models.ExcelSearchCriteria;
 import org.egov.cpt.models.RentDemandResponse;
 import org.egov.cpt.models.RequestInfoWrapper;
+import org.egov.cpt.service.ReadExcelNewFormatService;
 import org.egov.cpt.service.ReadExcelService;
 import org.egov.cpt.util.FileStoreUtils;
 import org.springframework.core.io.UrlResource;
@@ -25,11 +30,13 @@ public class ReadExcelController {
 
 	private ReadExcelService readExcelService;
 	private FileStoreUtils fileStoreUtils;
+	private ReadExcelNewFormatService readExcelNewFormatService;
 
-	public ReadExcelController(ReadExcelService readExcelService, FileStoreUtils fileStoreUtils) {
+	public ReadExcelController(ReadExcelService readExcelService, FileStoreUtils fileStoreUtils,ReadExcelNewFormatService readExcelNewFormatService) {
 		super();
 		this.readExcelService = readExcelService;
 		this.fileStoreUtils = fileStoreUtils;
+		this.readExcelNewFormatService = readExcelNewFormatService;
 	}
 
 	@PostMapping("/read")
@@ -39,8 +46,13 @@ public class ReadExcelController {
 		RentDemandResponse data = new RentDemandResponse();
 		try {
 			String filePath = fileStoreUtils.fetchFileStoreUrl(searchCriteria);
-			if (!"".equals(filePath))
-				data = readExcelService.getDatafromExcel(new UrlResource(filePath).getInputStream(), 0);
+			if (!"".equals(filePath)) {
+				if("0".equalsIgnoreCase(searchCriteria.getFileFormat())) {
+					data = readExcelService.getDatafromExcel(new UrlResource(filePath).getInputStream(), 0);
+				}else if("1".equalsIgnoreCase(searchCriteria.getFileFormat())) {
+					data = readExcelNewFormatService.getDatafromExcel(new UrlResource(filePath).getInputStream(), 0);
+				}
+			}
 			log.info("End controller method readExcel");
 		} catch (Exception e) {
 			log.error("Error occur during runnig controller method readExcel():" + e.getMessage());
