77c495fba4317d690cf1072c4eedf23c901e2103
Debjit Ghar <debjit.ghar@gmail.com>
2020-09-25 18:03:13 +0530

    Changes in rti and nulm sep

--- integration-services/src/main/java/org/egov/integration/service/RtiAsyncService.java
+++ integration-services/src/main/java/org/egov/integration/service/RtiAsyncService.java
@@ -30,12 +30,6 @@ public class RtiAsyncService {
 		        return null;
 		    }
 		    return res;
-		}).thenApplyAsync(result -> {
-			if(!StringUtils.isEmpty(result.getBody().getTransactionNo())){
-				RtiIndividualResponse res=RtiIndividualResponse.builder().transactionNo(result.getBody().getTransactionNo()).build();
-				rtiRestService.confirmTransaction(res,token);
-			}
-		    return result;
 		});
 		
 		CompletableFuture<ResponseEntity<RtiIndividualResponse>> nodalres = CompletableFuture.supplyAsync(() -> {
@@ -46,12 +40,6 @@ public class RtiAsyncService {
 		        return null;
 		    }
 		    return res;
-		}).thenApplyAsync(result -> {
-			if(!StringUtils.isEmpty(result.getBody().getTransactionNo())){
-				RtiIndividualResponse res=RtiIndividualResponse.builder().transactionNo(result.getBody().getTransactionNo()).build();
-				rtiRestService.confirmTransaction(res,token);
-			}
-		    return result;
 		});
 		
 		CompletableFuture<ResponseEntity<RtiIndividualResponse>> appellateres = CompletableFuture.supplyAsync(() -> {
@@ -62,28 +50,40 @@ public class RtiAsyncService {
 		        return null;
 		    }
 		    return res;
-		}).thenApplyAsync(result -> {
-			if(!StringUtils.isEmpty(result.getBody().getTransactionNo())){
-				RtiIndividualResponse res=RtiIndividualResponse.builder().transactionNo(result.getBody().getTransactionNo()).build();
-				rtiRestService.confirmTransaction(res,token);
-			}
-		    return result;
 		});
 		
         CompletableFuture.allOf(cpiores, nodalres, appellateres).join();
         
         RtiResponse response = new RtiResponse();
 		try {
-			if(null!=cpiores)
+			if(null!=cpiores) {
 				response.setCpio(cpiores.get().getBody());
-			if(null!=nodalres)
+				if(!StringUtils.isEmpty(cpiores.get().getBody().getTransactionNo())){
+					confirmAllTransactions(cpiores.get().getBody().getTransactionNo(),token);
+				}
+			}
+			if(null!=nodalres) {
 				response.setNodal(nodalres.get().getBody());
-			if(null!=appellateres)
+				if(!StringUtils.isEmpty(nodalres.get().getBody().getTransactionNo())){
+					confirmAllTransactions(nodalres.get().getBody().getTransactionNo(),token);
+				}
+			}
+			if(null!=appellateres) {
 				response.setAppellate(appellateres.get().getBody());
+				if(!StringUtils.isEmpty(appellateres.get().getBody().getTransactionNo())){
+					confirmAllTransactions(appellateres.get().getBody().getTransactionNo(),token);
+				}
+			}
 		} catch (InterruptedException | ExecutionException e) {					
 			throw new CustomException(CommonConstants.RTI_EXCEPTION_CODE, e.getMessage());
-		}
-		
+		}		
 		return response;
+	}
+	
+	public void confirmAllTransactions(String transactionNo, String token) {
+		CompletableFuture.runAsync(() -> {
+			RtiIndividualResponse res=RtiIndividualResponse.builder().transactionNo(transactionNo).build();
+			rtiRestService.confirmTransaction(res,token);
+		});		
 	}
 }
