7ef5b8fd49085da568392758106493fb20637401
Transerveshivam <68638560+Transerveshivam@users.noreply.github.com>
2020-11-03 17:43:01 +0530

    Feature/excel generation (#151)
    * xl generation code
    
    * code changes in excel generation
    
    * final code for account stmt excel generation
    
    * commented line removed

--- estate-services/src/main/java/org/egov/ps/util/FileStoreUtils.java
+++ estate-services/src/main/java/org/egov/ps/util/FileStoreUtils.java
@@ -1,12 +1,21 @@
 package org.egov.ps.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.egov.ps.model.ExcelSearchCriteria;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -49,6 +58,40 @@ public class FileStoreUtils {
 			log.error("No data could found by using this request: ", searchCriteria);
 		log.info("End - fetchFileStoreUrl, resposne : ", responseMap);
 		return responseMap;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<HashMap<String, String>> uploadStreamToFileStore(ByteArrayOutputStream outputStream, String tenantId,
+			String fileName, String contentType) throws UnsupportedEncodingException {
+		StringBuilder uri = new StringBuilder(fileStoreUrl.substring(0, fileStoreUrl.length() - 4));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+		body.add("filename", fileName);
+		body.add("contentType", contentType);
+
+		ByteArrayResource contentsAsResource = new ByteArrayResource(outputStream.toByteArray()) {
+			@Override
+			public String getFilename() {
+				return fileName; // Filename has to be returned in order to be able to post.
+			}
+		};
+		body.add("file", contentsAsResource);
+
+		uri.append("?tenantId=" + tenantId + "&module=" + "EstateServices");
+		try {
+			Map<String, Map<String, String>> response = (Map<String, Map<String, String>>) restTemplate
+					.postForObject(uri.toString(), requestEntity, HashMap.class);
+
+			List<HashMap<String, String>> result = (List<HashMap<String, String>>) response.get("files");
+			return result;
+		} catch (Exception e) {
+			log.error("Exception while fetching file store id: ", e);
+		}
+		return null;
 	}
 
 }
