2e6dafb7ee432be80cc52ed2f6432b1e862ecaa9
Extrapreneurs <60169213+extrapre@users.noreply.github.com>
2020-11-06 16:15:00 +0530

    NULM Renew Script Added

--- nulm-services/src/main/resources/db/migration/main/V202011051320320__nulm_susv_renew_create.sql
+++ nulm-services/src/main/resources/db/migration/main/V202011051320320__nulm_susv_renew_create.sql
@@ -1,7 +1,60 @@
-alter table public.nulm_suh_citizenngo_application add dob timestamp without time zone;
-alter table public.nulm_susv_application_detail add dob timestamp without time zone;
+CREATE TABLE public.nulm_susv_renew_application_detail
+(
+    application_uuid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    application_id character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    application_status character varying(64) COLLATE pg_catalog."default",
+    looking_for character varying(255) COLLATE pg_catalog."default",
+    name_of_street_vendor character varying(255) COLLATE pg_catalog."default",
+    cov_no character varying(255) COLLATE pg_catalog."default",
+    residential_address character varying(255) COLLATE pg_catalog."default",
+    change_of_location boolean,
+    proposed_address character varying(255) COLLATE pg_catalog."default",
+    tenant_id character varying(256) COLLATE pg_catalog."default",
+    is_active boolean,
+    created_by character varying(64) COLLATE pg_catalog."default",
+    created_time bigint,
+    last_modified_by character varying(64) COLLATE pg_catalog."default",
+    last_modified_time bigint,
+    CONSTRAINT nulm_susv_renew_application_detail_pkey PRIMARY KEY (application_uuid),
+    CONSTRAINT nulm_susv_renew_application_detail_application_id_key UNIQUE (application_id)
 
-alter table public.nulm_suh_occupancy_log add dob timestamp without time zone;
+)
 
+CREATE TABLE public.nulm_susv_renew_application_document
+(
+    document_uuid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    filestore_id character varying COLLATE pg_catalog."default" NOT NULL,
+    application_uuid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    document_type character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    tenant_id character varying(256) COLLATE pg_catalog."default",
+    is_active boolean NOT NULL,
+    created_by character varying(64) COLLATE pg_catalog."default",
+    created_time bigint,
+    last_modified_by character varying(256) COLLATE pg_catalog."default",
+    last_modified_time bigint,
+    CONSTRAINT nulm_susv_renew_application_document_pkey PRIMARY KEY (document_uuid),
+    CONSTRAINT "FK_nulm_susv_renew_application_document_application_uuid" FOREIGN KEY (application_uuid)
+        REFERENCES public.nulm_susv_renew_application_detail (application_uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
-alter table public.nulm_smid_application_detail add age integer;
+CREATE TABLE public.nulm_susv_renew_familiy_detail
+(
+    uuid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    application_uuid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(64) COLLATE pg_catalog."default",
+    age character varying(64) COLLATE pg_catalog."default",
+    relation character varying(64) COLLATE pg_catalog."default",
+    tenant_id character varying(256) COLLATE pg_catalog."default",
+    is_active boolean,
+    created_by character varying(64) COLLATE pg_catalog."default",
+    created_time bigint,
+    last_modified_by character varying(64) COLLATE pg_catalog."default",
+    last_modified_time bigint,
+    CONSTRAINT nulm_susv_renew_familiy_detail_pkey PRIMARY KEY (uuid),
+    CONSTRAINT "FK_nulm_susv_renew_familiy_detail_application_uuid" FOREIGN KEY (application_uuid)
+        REFERENCES public.nulm_susv_renew_application_detail (application_uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
