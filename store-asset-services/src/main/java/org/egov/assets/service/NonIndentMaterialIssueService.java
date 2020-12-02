d125aee001b700f71f3e843b4d419753e1acd642
Extrapreneurs <60169213+extrapre@users.noreply.github.com>
2020-12-02 10:02:37 +0530

    Store Asset Issues Non Indent Fixed

--- store-asset-services/src/main/java/org/egov/assets/service/NonIndentMaterialIssueService.java
+++ store-asset-services/src/main/java/org/egov/assets/service/NonIndentMaterialIssueService.java
@@ -1127,14 +1127,13 @@ public class NonIndentMaterialIssueService extends DomainService {
 	public MaterialIssueResponse updateStatus(MaterialIssueRequest indentIssueRequest) {
 
 		try {
-//			WorkFlowDetails workFlowDetails = workflowIntegrator.callWorkFlow(indentIssueRequest.getRequestInfo(),
-//					indentIssueRequest.getWorkFlowDetails(), indentIssueRequest.getWorkFlowDetails().getTenantId());
-//			indentIssueRequest.setWorkFlowDetails(workFlowDetails);
-//			kafkaQue.send(updateStatusTopic, updateStatusKey, indentIssueRequest);
+			WorkFlowDetails workFlowDetails = workflowIntegrator.callWorkFlow(indentIssueRequest.getRequestInfo(),
+					indentIssueRequest.getWorkFlowDetails(), indentIssueRequest.getWorkFlowDetails().getTenantId());
+			indentIssueRequest.setWorkFlowDetails(workFlowDetails);
+			kafkaQue.send(updateStatusTopic, updateStatusKey, indentIssueRequest);
 			MaterialIssueResponse response = new MaterialIssueResponse();
 			response.setMaterialIssues(indentIssueRequest.getMaterialIssues());
 			response.setResponseInfo(getResponseInfo(indentIssueRequest.getRequestInfo()));
-			 indentIssueRequest.getWorkFlowDetails().setStatus("Approved");
 			if (indentIssueRequest.getWorkFlowDetails().getStatus()
 					.equals(MaterialIssueStatusEnum.REJECTED.toString())) {
 				MaterialIssueSearchContract searchContract = new MaterialIssueSearchContract(
