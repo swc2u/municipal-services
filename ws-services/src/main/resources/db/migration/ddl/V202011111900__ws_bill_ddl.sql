8c84be659db932fe6cdbbff1373a0707dd6e22e6
Extrapre <extrapre@extrapreneursindia.com>
2021-01-06 11:25:12 +0530

    added db scripts

--- ws-services/src/main/resources/db/migration/ddl/V202011111900__ws_bill_ddl.sql
+++ ws-services/src/main/resources/db/migration/ddl/V202011111900__ws_bill_ddl.sql
@@ -38,3 +38,56 @@ CREATE TABLE eg_ws_savebilling
   CONSTRAINT eg_ws_savebilling_pkey PRIMARY KEY (id)
 );
 
+
+CREATE TABLE eg_ws_billfile_history
+(
+  id character varying(64) NOT NULL,
+  filestore_url character varying(250) NOT NULL,
+  filestore_id character varying(64) NOT NULL,
+  filegeneration_time bigint,
+  CONSTRAINT eg_ws_billfile_history_pkey PRIMARY KEY (id)
+);
+
+----------
+
+ALTER TABLE eg_ws_connection  
+ADD COLUMN cccode varchar,
+ADD COLUMN div varchar,
+ADD COLUMN subdiv varchar,
+ADD COLUMN ledger_no varchar;
+
+----------------
+
+ALTER TABLE eg_ws_savebilling
+ADD COLUMN totalAmount_paid varchar,
+add column paymentMode varchar,
+add column isFileGenerated boolean;
+
+---------------
+
+create table eg_ws_savebilling_audit as (select * from eg_ws_savebilling where 1=2);
+
+----------------
+CREATE TRIGGER update_bill_trigger 
+BEFORE UPDATE OF consumercode ON eg_ws_savebilling
+FOR EACH ROW 
+EXECUTE PROCEDURE insert_billing_audit_data();
+
+----------------
+
+
+CREATE OR REPLACE FUNCTION insert_billing_audit_data()
+  RETURNS trigger AS
+$$
+BEGIN
+
+    INSERT INTO eg_ws_savebilling_audit ( id, cccode, divsdiv, consumercode, billcycle, billgroup, subgroup, billtype, name, address, cesscharge, netamount, grossamount, surcharge, totalnetamount, totalsurcharge, totalgrossamount, fixchargecode, fixcharge, duedatecash, duedatecheque, status, billid, paymentid, paymentstatus,totalAmount_paid,paymentMode,isFileGenerated, createdby, createdtime,lastmodifiedby,lastmodifiedtime )
+         VALUES(OLD.id,OLD.cccode,OLD.divsdiv,OLD.consumercode,OLD.billcycle,OLD.billgroup,OLD.subgroup,OLD.billtype,OLD.name,OLD.address,OLD.cesscharge,OLD.netamount,OLD.grossamount,OLD.surcharge,OLD.totalnetamount,OLD.totalsurcharge,OLD.totalgrossamount,OLD.fixchargecode,OLD.fixcharge,OLD.duedatecash,OLD.duedatecheque,OLD.status,OLD.billid,OLD.paymentid,OLD.paymentstatus,old.totalAmount_paid,old.paymentMode,old.isFileGenerated, old.createdby,old.createdtime,old.lastmodifiedby,old.lastmodifiedtime);
+
+RETURN OLD;
+
+END;
+
+$$
+
+LANGUAGE 'plpgsql';
