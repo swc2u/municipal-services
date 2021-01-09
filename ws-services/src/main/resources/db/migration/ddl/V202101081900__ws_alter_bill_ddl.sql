d8a498cec5fbe6080d9a5dbac93852c6133a794d
root <root@portal-qa.ifcs5pwc2yuu3elqmvyvxiieba.rx.internal.cloudapp.net>
2021-01-09 11:59:03 +0000

    Changed for ws changes

--- ws-services/src/main/resources/db/migration/ddl/V202101081900__ws_alter_bill_ddl.sql
+++ ws-services/src/main/resources/db/migration/ddl/V202101081900__ws_alter_bill_ddl.sql
@@ -1,5 +1,4 @@
-
-
+DROP TABLE IF EXISTS eg_ws_billfile_history;
 CREATE TABLE eg_ws_billfile_history
 (
   id character varying(64) NOT NULL,
@@ -9,7 +8,6 @@ CREATE TABLE eg_ws_billfile_history
   CONSTRAINT eg_ws_billfile_history_pkey PRIMARY KEY (id)
 );
 
-----------
 
 ALTER TABLE eg_ws_connection  
 ADD COLUMN cccode varchar,
@@ -17,15 +15,13 @@ ADD COLUMN div varchar,
 ADD COLUMN subdiv varchar,
 ADD COLUMN ledger_no varchar;
 
-----------------
 
 ALTER TABLE eg_ws_savebilling
 ADD COLUMN totalAmount_paid varchar,
 add column paymentMode varchar,
 add column isFileGenerated boolean;
 
----------------
-
+DROP TABLE IF EXISTS eg_ws_savebilling_audit;
 CREATE TABLE eg_ws_savebilling_audit
 (
   id character varying(64) not null,
@@ -66,29 +62,22 @@ CREATE TABLE eg_ws_savebilling_audit
   paymentMode varchar,
   isFileGenerated boolean
 );
------------------
 
-CREATE OR REPLACE FUNCTION insert_billing_audit_data()
-  RETURNS trigger AS
+CREATE OR REPLACE FUNCTION insert_ws_billing_audit_data()
+  RETURNS trigger
+  LANGUAGE plpgsql
+AS
 $$
 BEGIN
-
     INSERT INTO eg_ws_savebilling_audit ( id, cccode, divsdiv, consumercode, billcycle, billgroup, subgroup, billtype, name, address, cesscharge, netamount, grossamount, surcharge, totalnetamount, totalsurcharge, totalgrossamount, fixchargecode, fixcharge, duedatecash, duedatecheque, status, billid, paymentid, paymentstatus,totalAmount_paid,paymentMode,isFileGenerated, createdby, createdtime,lastmodifiedby,lastmodifiedtime )
          VALUES(OLD.id,OLD.cccode,OLD.divsdiv,OLD.consumercode,OLD.billcycle,OLD.billgroup,OLD.subgroup,OLD.billtype,OLD.name,OLD.address,OLD.cesscharge,OLD.netamount,OLD.grossamount,OLD.surcharge,OLD.totalnetamount,OLD.totalsurcharge,OLD.totalgrossamount,OLD.fixchargecode,OLD.fixcharge,OLD.duedatecash,OLD.duedatecheque,OLD.status,OLD.billid,OLD.paymentid,OLD.paymentstatus,old.totalAmount_paid,old.paymentMode,old.isFileGenerated, old.createdby,old.createdtime,old.lastmodifiedby,old.lastmodifiedtime);
 
 RETURN OLD;
-
 END;
+$$;
 
-$$
-
-LANGUAGE 'plpgsql';
-
-----------------
-CREATE TRIGGER update_bill_trigger 
-BEFORE UPDATE OF consumercode ON eg_ws_savebilling
+DROP TRIGGER IF EXISTS update_ws_bill_trigger ON eg_ws_savebilling;
+CREATE TRIGGER update_ws_bill_trigger 
+BEFORE UPDATE ON eg_ws_savebilling
 FOR EACH ROW 
-EXECUTE PROCEDURE insert_billing_audit_data();
-
-----------------
-
+EXECUTE PROCEDURE insert_ws_billing_audit_data();
