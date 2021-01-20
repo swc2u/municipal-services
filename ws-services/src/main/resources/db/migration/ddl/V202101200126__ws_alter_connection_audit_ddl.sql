1ed0d2168fbfb6f5df84b8d550d20d88ca0d7664
Extrapre <extrapre@extrapreneursindia.com>
2021-01-20 16:03:46 +0530

    changed column's varchar length

--- ws-services/src/main/resources/db/migration/ddl/V202101200126__ws_alter_connection_audit_ddl.sql
+++ ws-services/src/main/resources/db/migration/ddl/V202101200126__ws_alter_connection_audit_ddl.sql
@@ -1,16 +1,31 @@
+ALTER TABLE eg_ws_connection 
+alter COLUMN cccode type varchar(8),
+alter COLUMN ledger_no type varchar(8),
+alter COLUMN subdiv type varchar(8),
+alter COLUMN ledgergroup type varchar(8),
+alter COLUMN div type  varchar(8);
+
+
+ALTER TABLE eg_ws_service 
+alter COLUMN metercount type varchar(64),
+alter COLUMN meterrentcode type varchar,
+alter COLUMN mfrcode type varchar(32),
+alter COLUMN meterdigits type varchar(64),
+alter COLUMN sanctionedcapacity type varchar(128),
+alter COLUMN meterunit type  varchar(64);
 
 ALTER TABLE eg_ws_connection_audit  
-ADD COLUMN cccode character varying,
-ADD COLUMN div character varying,
-ADD COLUMN subdiv character varying,
-ADD COLUMN ledger_no character varying,
-ADD COLUMN ledgergroup character varying;
+ADD COLUMN cccode character varying(8),
+ADD COLUMN div character varying(8),
+ADD COLUMN subdiv character varying(8),
+ADD COLUMN ledger_no character varying(8),
+ADD COLUMN ledgergroup character varying(8);
 
 
 ALTER TABLE eg_ws_service_audit  
-ADD COLUMN meterCount varchar,
-ADD COLUMN meterRentCode varchar,
-ADD COLUMN mfrCode varchar,
-ADD COLUMN meterDigits varchar,
-ADD COLUMN meterUnit varchar,   
-ADD COLUMN sanctionedCapacity varchar ;
+ADD COLUMN meterCount varchar(64),
+ADD COLUMN meterRentCode varchar(64),
+ADD COLUMN mfrCode varchar(32),
+ADD COLUMN meterDigits varchar(64),
+ADD COLUMN meterUnit varchar(64),   
+ADD COLUMN sanctionedCapacity varchar(128) ;
