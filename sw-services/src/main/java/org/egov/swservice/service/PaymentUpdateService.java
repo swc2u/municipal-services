dd12040679722a78e25625c232b0c4ca2830dc73
Rushikesh <echallan123@gmail.com>
2021-06-10 18:08:44 +0530

    ws-service reconcillation changes

--- sw-services/src/main/java/org/egov/swservice/service/PaymentUpdateService.java
+++ sw-services/src/main/java/org/egov/swservice/service/PaymentUpdateService.java
@@ -75,8 +75,10 @@ public class PaymentUpdateService {
 			} catch (Exception ex) {
 				log.error("Temp Catch Excption:", ex);
 			}
+			if(!"EARLY_RECONC_JOB".equalsIgnoreCase(paymentRequest.getRequestInfo().getUserInfo().getUuid())) {
 			paymentRequest.getRequestInfo().setUserInfo(fetchUser(
 					paymentRequest.getRequestInfo().getUserInfo().getUuid(), paymentRequest.getRequestInfo()));
+			}
 			for (PaymentDetail paymentDetail : paymentRequest.getPayment().getPaymentDetails()) {
 				log.info("Consuming Business Service", paymentDetail.getBusinessService());
 				if (paymentDetail.getBusinessService().equalsIgnoreCase(config.getReceiptBusinessservice())) {
